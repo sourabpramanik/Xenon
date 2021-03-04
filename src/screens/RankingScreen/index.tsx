e9bbb6e43984f844c0673fa2acc86a52a80e9596
Sourab Pramanik <sourabpramanik2011@outlook.com>
2021-03-04 22:28:46 +0530

    Structure initialized

---
+++ src/screens/RankingScreen/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from "react-native";
+
+const RankingScreen = () => {
+    return(
+        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <Text style={{color:'white'}}>Ranking Components</Text>
+        </View>
+    )
+}
+
+export default RankingScreen;
